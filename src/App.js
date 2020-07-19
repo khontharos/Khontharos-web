8a89032bace37f82d90b9d07ec270c642fab8226
khontharos <khontharos.bb@gmail.com>
2020-07-19 12:35:37 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Profile from "./Components/Profile";
+
+function App() 
+return
+
+
+function App() {
+  return (
+    <div className="App"
+     
+ );
+}
+
+export default App;
